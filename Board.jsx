8e655fa2772225b1e4d8304a5f19d23da8feebe6
tssnainital-byte <tssnainital@gmail.com>
2026-03-15 02:45:31 +0530

    Add carousel component to Board.jsx
    Hello

---
+++ Board.jsx
@@ -0,0 +1,17 @@
+
+  <br></br><br></br>
+  
+   <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Hostel_Mess_small.jpg" class="d-block w-100" height="430px" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://content.jdmagicbox.com/comp/def_content/buffet-restaurants/buffet-restaurants3-buffet-restaurants-3-xlink.jpg" class="d-block w-100" height="430px" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.ytimg.com/vi/M_BRmqP67_I/maxresdefault.jpg" class="d-block w-100" height="430px" alt="..."/>
+    </div>
+     <div class="carousel-item">
+      <img src="https://images.shiksha.com/mediadata/images/1531467787phpPCTRIq.jpeg" class="d-block w-100" height="430px" alt="..."/>
+    </div
